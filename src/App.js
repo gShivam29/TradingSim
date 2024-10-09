e391404cdd8c3c7f9ae7da331d9b8c7cb742012a
gShivam29 <shivamsgupta29@gmail.com>
2024-10-09 22:32:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import "./styles/app.css";
+import { createChart } from "lightweight-charts";
 
 function App() {
+  const chartContainerRef = useRef(); // Create a ref to store the chart container
+
+  useEffect(() => {
+    const chart = createChart(chartContainerRef.current, {
+      width: 1200,
+      height: 600,
+      timeScale: {
+        timeVisible: true,
+        secondVisible: false,
+      },
+    });
+
+    const candleSeries = chart.addCandlestickSeries();
+
+    fetch(
+      "https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1m&limit=1000"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const cdata = data.map((d) => {
+          return {
+            time: d[0] / 1000,
+            open: parseFloat(d[1]),
+            high: parseFloat(d[2]),
+            low: parseFloat(d[3]),
+            close: parseFloat(d[4]),
+          };
+        });
+        candleSeries.setData(cdata);
+      })
+      .catch((err) => console.log(err));
+
+    return () => {
+      chart.remove(); // Cleanup the chart on unmount
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div ref={chartContainerRef} style={{ position: "relative", width: "500", height: "00" }} />
   );
 }
 
---
+++ src/components/Topbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../styles/topbar.css";
+import logo from '../assets/images/mylogo.jpg'
+const Topbar = () => {
+  return (
+    <header className='header'>
+      <img src={logo} className='navbar-logo' />
+
+      <nav className='navbar'>
+        <a href='/'>Explore</a>
+        <a href='/'>Dashboard</a>
+        <a href='/'>Profile</a>
+
+      </nav>
+    </header>
+  )
+}
+export default Topbar
